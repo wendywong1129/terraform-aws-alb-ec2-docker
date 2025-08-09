b30b03ef4467d177c0159968b5901a2ff6ead293
Wendy <wendywong.qw@gmail.com>
2025-08-09 11:17:00 +1000

    Add output variable showing bastion host public IP

--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -7,3 +7,8 @@ output "private_ip" {
   description = "The private IP address of the EC2 instance"
   value       = aws_instance.ec2_instance.private_ip
 }
+
+output "public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.ec2_instance.public_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "alb_dns_name" {
   value       = module.alb.dns_name
 }
 
+output "bastion_public_ip" {
+  description = "Public IP address of the Bastion Host"
+  value       = module.bastion_host.public_ip
+}
+
 output "app_server_private_ip" {
   description = "Private IP address of the App Server"
   value       = module.app_server.private_ip
